8569bce2b6fd742271fb95f3ebe95b48005b3d43
Eliardo <eliardosilva81@gmail.com>
2025-07-23 16:34:36 -0300

    feat: treinando o padrao delegate

---
+++ Quiz/ViewController/QuestionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  QuestionViewController.swift
+//  Quiz
+//
+//  Created by Eliardo Venancio on 22/07/25.
+//
+
+import UIKit
+
+protocol QuestionViewControllerDelegate: AnyObject {
+    func questionView(_ view: QuestionViewController, didSelect answer: String)
+}
+
+class QuestionViewController: UIViewController {
+    
+}
